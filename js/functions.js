9113817ace3be5db9c43cc7a2fb2646ff4bbddc3
jarlhelin <jarl.helin@live.fr>
2014-02-27 06:14:44 +0100

    Added tabs in TECHNICAL SUPPORT

--- js/functions.js
+++ js/functions.js
@@ -53,40 +53,50 @@ head.ready(function() {
 		//btn
 		$('header ul li:nth-child(2)').click(function(){
 				$(this).addClass('clicked', 200).siblings().removeClass('clicked');
+				$(this).parents().children().find('header ul li:nth-child(5)').addClass('pull', 200);
 				//quicklinks 
-			$(this).parents().children().find('.quicklink-menu').addClass('home', 1000);
-			$(this).parents().children().find('.main').addClass('push', 200);
+			//$(this).parents().children().find('.quicklink-menu').addClass('home', 1000);
+			//$(this).parents().children().find('.main').addClass('push', 200);
 			//other fields invisible
+			$(this).parents().children().find('.quicklink-menu').removeClass('home');
 			$(this).parents().children().find('.category-menu').addClass('invisible');
 			$(this).parents().children().find('.orbit-container').addClass('invisible');
 			$(this).parents().children().find('.orbit-bullets-container').addClass('invisible');
 		});
 		$('header ul li:nth-child(3)').click(function(){
 				$(this).addClass('clicked', 200).siblings().removeClass('clicked');
+				$(this).parents().children().find('header ul li:nth-child(5)').addClass('pull', 200);
 				//quicklinks 
-			$(this).parents().children().find('.quicklink-menu').addClass('home', 1000);
-			$(this).parents().children().find('.main').addClass('push', 200);
+			//$(this).parents().children().find('.quicklink-menu').addClass('home', 1000);
+			//$(this).parents().children().find('.main').addClass('push', 200);
 			//other fields invisible
+			$(this).parents().children().find('.quicklink-menu').removeClass('home');
 			$(this).parents().children().find('.category-menu').addClass('invisible');
 			$(this).parents().children().find('.orbit-container').addClass('invisible');
 			$(this).parents().children().find('.orbit-bullets-container').addClass('invisible');
 		});
 		$('header ul li:nth-child(4)').click(function(){
 				$(this).addClass('clicked', 200).siblings().removeClass('clicked');
+				$(this).parents().children().find('header ul li:nth-child(5)').addClass('pull', 200);
 				//quicklinks 
-			$(this).parents().children().find('.quicklink-menu').addClass('home', 1000);
-			$(this).parents().children().find('.main').addClass('push', 200);
+			//$(this).parents().children().find('.quicklink-menu').addClass('home', 1000);
+			//$(this).parents().children().find('.main').addClass('push', 200);
 			//other fields invisible
+			$(this).parents().children().find('.quicklink-menu').removeClass('home');
 			$(this).parents().children().find('.category-menu').addClass('invisible');
 			$(this).parents().children().find('.orbit-container').addClass('invisible');
 			$(this).parents().children().find('.orbit-bullets-container').addClass('invisible');
 		});
+		$('header ul li:nth-child(5)').click(function(){
+		});
 		$('header ul li:nth-child(6)').click(function(){
 				$(this).addClass('clicked', 200).siblings().removeClass('clicked');
+				$(this).parents().children().find('header ul li:nth-child(5)').addClass('pull', 200);
 				//quicklinks 
-			$(this).parents().children().find('.quicklink-menu').addClass('home', 1000);
-			$(this).parents().children().find('.main').addClass('push', 200);
+			//$(this).parents().children().find('.quicklink-menu').addClass('home', 1000);
+			//$(this).parents().children().find('.main').addClass('push', 200);
 			//other fields invisible
+			$(this).parents().children().find('.quicklink-menu').removeClass('home');
 			$(this).parents().children().find('.category-menu').addClass('invisible');
 			$(this).parents().children().find('.orbit-container').addClass('invisible');
 			$(this).parents().children().find('.orbit-bullets-container').addClass('invisible');
@@ -104,8 +114,28 @@ head.ready(function() {
 		$('header ul li:nth-child(6)').click(function(){
 			$(this).parents().children().find('.header-drop li.fourth').addClass('active').siblings().removeClass('active');
 		});
-	
-	/*
+		//Modal
+		//Statement <1024 Toggle loads of classes
+		//refresh page on browser resize
+		
+		//refresh page on browser resize
+		/*$(window).bind('resize', function(e)
+		//{
+		  //if (window.RT) clearTimeout(window.RT);
+		  //window.RT = setTimeout(function()
+		  //{
+		    //this.location.reload(false); /* false to get page from cache */
+		  //}, 200);
+		//});*/
+		//end refresh
+		/*
+		if ($(window).width() < 1024) {
+		   alert('Less than 1024 and toggle classes');
+		}
+		else {
+		   alert('More than 960');
+		}*/
+		/*
 		if(condition) {
 		// code to be executed if condition is true
 		} 
