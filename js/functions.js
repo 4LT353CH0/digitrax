8ba115380dbf68882e4f901aa07cfcdaee7b758b
jarlhelin <jarl.helin@live.fr>
2014-03-03 02:09:57 +0100

    Halfway through pages.. Needs page-personal/page-cart/page-checkout

--- js/functions.js
+++ js/functions.js
@@ -3,104 +3,36 @@ head.ready(function() {
 	
 	//Initialize foundation JavaScript 
 	$(document).foundation();
-	//HOME quicklinks
-	$( ".quicklink-btn:nth-child(1)" ).click(function() {
-		$(this).addClass('invisible');
-		$(this).parents().children().find('.quicklink-btn:nth-child(2)').removeClass('invisible');
-		$(this).parents().children().find('.main').addClass('push', 200);
-		$(this).parents().children().find('.quicklink-menu').addClass('active', 200);
-	});
-	$( ".quicklink-btn:nth-child(2)" ).click(function() {
-		$(this).addClass('invisible');
-		$(this).parents().children().find('.quicklink-btn:nth-child(1)').removeClass('invisible');
-		$(this).parents().children().find('.main').removeClass('push', 200);
-		$(this).parents().children().find('.quicklink-menu').removeClass('active', 200);
-		$(this).parents().children().find('.quicklink-menu').removeClass('home');
-	});
-	//Click back HOME
-		$('.quicklink-menu li:nth-child(1)').click(function(){
-		$(this).parents().children().find('.quicklink-menu').removeClass('home');
-		$(this).parents().children().find('.quicklink-menu').removeClass('active', 200);
-		$(this).parents().children().find('.main').removeClass('push', 200);
-		//header
-		$(this).parents().children().find('header ul li').removeClass('clicked');
-		//drop`
-		$(this).parents().children().find('.header-drop li').removeClass('active');
-		//other fields
-		$(this).parents().children().find('.orbit-container').removeClass('invisible');
-		$(this).parents().children().find('.orbit-bullets-container').removeClass('invisible');
-		$(this).parents().children().find('.category-menu').addClass('invisible');
 	
-	});
-	//Click categories
-	$('.categories-btn').click(function(){
-			if ($(this).parents().children().find('.category-menu').hasClass('invisible')) {
-      			$(this).parents().children().find('.category-menu').toggleClass('invisible');
-				$(this).parents().children().find('.main').addClass('push', 200);
-				$(this).parents().children().find('.orbit-container').addClass('invisible');
-				$(this).parents().children().find('.orbit-bullets-container').addClass('invisible');
-				$(this).parents().children().find('.quicklink-menu').addClass('home', 200);
-				//Header
-				$(this).parents().children().find('.header-drop li').removeClass('active');
-				//header
-				$(this).parents().children().find('header ul li').removeClass('clicked');
-    		} else {
-    		}
-
-		});
 	//Click header dropdown
 	
 		//btn
 		$('header ul li:nth-child(2)').click(function(){
 				$(this).addClass('clicked', 200).siblings().removeClass('clicked');
-				$(this).parents().children().find('header ul li:nth-child(5)').addClass('pull', 200);
-				//quicklinks 
-			//$(this).parents().children().find('.quicklink-menu').addClass('home', 1000);
-			//$(this).parents().children().find('.main').addClass('push', 200);
 			//other fields invisible
-			$(this).parents().children().find('.quicklink-menu').removeClass('home');
-			$(this).parents().children().find('.category-menu').addClass('invisible');
 			$(this).parents().children().find('.orbit-container').addClass('invisible');
 			$(this).parents().children().find('.orbit-bullets-container').addClass('invisible');
 		});
 		$('header ul li:nth-child(3)').click(function(){
 				$(this).addClass('clicked', 200).siblings().removeClass('clicked');
-				$(this).parents().children().find('header ul li:nth-child(5)').addClass('pull', 200);
-				//quicklinks 
-			//$(this).parents().children().find('.quicklink-menu').addClass('home', 1000);
-			//$(this).parents().children().find('.main').addClass('push', 200);
 			//other fields invisible
-			$(this).parents().children().find('.quicklink-menu').removeClass('home');
-			$(this).parents().children().find('.category-menu').addClass('invisible');
 			$(this).parents().children().find('.orbit-container').addClass('invisible');
 			$(this).parents().children().find('.orbit-bullets-container').addClass('invisible');
 		});
 		$('header ul li:nth-child(4)').click(function(){
 				$(this).addClass('clicked', 200).siblings().removeClass('clicked');
-				$(this).parents().children().find('header ul li:nth-child(5)').addClass('pull', 200);
-				//quicklinks 
-			//$(this).parents().children().find('.quicklink-menu').addClass('home', 1000);
-			//$(this).parents().children().find('.main').addClass('push', 200);
 			//other fields invisible
-			$(this).parents().children().find('.quicklink-menu').removeClass('home');
-			$(this).parents().children().find('.category-menu').addClass('invisible');
 			$(this).parents().children().find('.orbit-container').addClass('invisible');
 			$(this).parents().children().find('.orbit-bullets-container').addClass('invisible');
-		});
-		$('header ul li:nth-child(5)').click(function(){
 		});
 		$('header ul li:nth-child(6)').click(function(){
 				$(this).addClass('clicked', 200).siblings().removeClass('clicked');
-				$(this).parents().children().find('header ul li:nth-child(5)').addClass('pull', 200);
-				//quicklinks 
-			//$(this).parents().children().find('.quicklink-menu').addClass('home', 1000);
-			//$(this).parents().children().find('.main').addClass('push', 200);
 			//other fields invisible
-			$(this).parents().children().find('.quicklink-menu').removeClass('home');
-			$(this).parents().children().find('.category-menu').addClass('invisible');
 			$(this).parents().children().find('.orbit-container').addClass('invisible');
 			$(this).parents().children().find('.orbit-bullets-container').addClass('invisible');
 		});
+		
+	
 		//condition dans l'onglet et ouvrir bloc
 		$('header ul li:nth-child(2)').click(function(){
 			$(this).parents().children().find('.header-drop li.first').addClass('active').siblings().removeClass('active');
@@ -114,6 +46,14 @@ head.ready(function() {
 		$('header ul li:nth-child(6)').click(function(){
 			$(this).parents().children().find('.header-drop li.fourth').addClass('active').siblings().removeClass('active');
 		});
+		//scrollTo
+		$('.up').click(function(){
+			$(this).parents().children().find('.main').stop().scrollTo( '-=400px', 800);
+		});
+		$('.down').click(function(){
+			$(this).parents().children().find('.main').stop().scrollTo( '+=400px', 800);
+		});
+
 		//Modal
 		//Statement <1024 Toggle loads of classes
 		//refresh page on browser resize
